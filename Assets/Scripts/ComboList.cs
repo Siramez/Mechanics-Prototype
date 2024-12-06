c5b3705143a5095f1122c72c5f8f53b16c62971d
Siramez <yunusemrearisoy123@gmail.com>
2024-12-06 15:32:14 +0000

    mechanic prototype

---
+++ Assets/Scripts/ComboList.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ComboList", menuName = "ScriptableObjects/ComboList", order = 1)]
+public class ComboList : ScriptableObject
+{
+    public ComboData[] combos;
+    public bool isMelee;
+}
